2ea2f113d672b13d419e478244d61090db0d6edc
kushalbhandari1234 <kushalbhandari9998@gmail.com>
2025-11-19 16:33:46 +0545

    updated

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-//variable declaration 
- let name = "kushal";
-  console.log(name);
- const fruits = ["apple" , " banana" , "cherry"];
- console.log(fruits[])
+function greater(a,b,c)
+{
+
+if(a>b && a>c){
+
+console.log(`the number ${a} is greater`);
+}
+else if(b>c && b>a){
+    console.log(`the number ${b} is greater`);
+
+}
+
+else{
+    console.log(`The number ${c} is greater `)
+}
+
+return a,b,c ;
+}
+
+greater(11,3,6)
