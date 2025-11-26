d8b3ffe200eeba1b881b61a6fc1c9750467f0679
kushalbhandari1234 <kushalbhandari9998@gmail.com>
2025-11-26 15:53:18 +0545

    updated

---
+++ Arrow.js
@@ -0,0 +1,13 @@
+//arrow function
+
+const greater = (x,y) => {
+ if(x>y){
+    console.log(`the number x is greater`);
+ }
+ 
+ else{
+    console.log(`the number y is greater `);
+ }
+}
+
+greater(5,3)
