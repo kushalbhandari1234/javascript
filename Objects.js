2a2da2326e288045e87fed57c8b3e67ed5b6fda1
kushalbhandari1234 <kushalbhandari9998@gmail.com>
2025-11-26 16:09:59 +0545

    updated

---
+++ Objects.js
@@ -0,0 +1,12 @@
+//object
+
+const person = {
+name : "madara",
+age : 16 ,
+hobbies : ["gaming","working out","music"]
+
+}
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.hobbies[1]);
