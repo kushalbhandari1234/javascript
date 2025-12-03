e14149c6e5c2ffcb6b462180d9ffed22fbecdb87
kushalbhandari1234 <kushalbhandari9998@gmail.com>
2025-12-03 16:01:38 +0545

    updated

---
+++ map1.js
@@ -0,0 +1,5 @@
+const num = [2,3,5,6];
+
+const numbers = num.map(num => 2* num);
+
+console.log(numbers);
---
+++ map2.js
@@ -0,0 +1,7 @@
+console.log("niggazzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz")
+
+const names = ["Kushal:]" ,  "nausterrafu:]" , "saugandu:]"];
+ 
+const teamseven = names.map(name =>name.toUpperCase());
+
+console.log(teamseven);
